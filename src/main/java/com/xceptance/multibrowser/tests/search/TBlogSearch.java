eb51158da3d7ec964d3d8fea6170b4e2f4e59f03
Marcus Kaufmann <m.kaufmann@xceptance.net>
2017-08-21 12:47:43 +0200

    code formatted

--- src/main/java/com/xceptance/multibrowser/tests/search/TBlogSearch.java
+++ src/main/java/com/xceptance/multibrowser/tests/search/TBlogSearch.java
@@ -2,12 +2,14 @@
  * NOTE: This file is generated. Do not edit! Your changes will be lost.
  */
 package com.xceptance.multibrowser.tests.search;
+
 import com.xceptance.xlt.api.engine.scripting.AbstractScriptTestCase;
 import com.xceptance.xlt.api.engine.scripting.ScriptName;
 
-
 /**
- * <p>This is a simple demo for a Google search and will demonstrate how the multi browser suite works.</p>
+ * <p>
+ * This is a simple demo for a Google search and will demonstrate how the multi browser suite works.
+ * </p>
  */
 @ScriptName("tests.search.TBlogSearch")
 public class TBlogSearch extends AbstractScriptTestCase
