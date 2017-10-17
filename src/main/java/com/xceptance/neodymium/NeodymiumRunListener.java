20b9259bf84c23cec7fe7b8b50d053d5f4f84ad9
Marcus Kaufmann <m.kaufmann@xceptance.net>
2017-10-17 16:36:24 +0200

    added after test hook and logging

--- src/main/java/com/xceptance/neodymium/NeodymiumRunListener.java
+++ src/main/java/com/xceptance/neodymium/NeodymiumRunListener.java
@@ -1,15 +1,23 @@
 package com.xceptance.neodymium;
 
+import org.junit.runner.Result;
 import org.junit.runner.notification.Failure;
 import org.junit.runner.notification.RunListener;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.xceptance.neodymium.multibrowser.BrowserRunner;
 
 public class NeodymiumRunListener extends RunListener
 {
+    private static final Logger LOGGER = LoggerFactory.getLogger(NeodymiumRunListener.class);
+
     private Failure failure = null;
 
     @Override
     public void testFailure(Failure failure) throws Exception
     {
+        LOGGER.debug("Test failed: " + failure);
         this.failure = failure;
     }
 
@@ -21,6 +29,13 @@ public class NeodymiumRunListener extends RunListener
     public Failure getFailure()
     {
         return failure;
+    }
+
+    @Override
+    public void testRunFinished(Result result) throws Exception
+    {
+        LOGGER.debug("All tests finished. Quit cached Browser!");
+        BrowserRunner.quitCachedBrowser();
     }
 
 }
