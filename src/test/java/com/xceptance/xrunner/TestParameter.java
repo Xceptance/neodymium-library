daef35924b7e3ea131e20c42b23126361cfce029
Marcus Kaufmann <m.kaufmann@xceptance.net>
2017-11-02 15:39:15 +0100

    broken code

--- src/test/java/com/xceptance/xrunner/TestParameter.java
+++ src/test/java/com/xceptance/xrunner/TestParameter.java
@@ -8,7 +8,6 @@ import org.junit.runners.Parameterized.Parameter;
 import org.junit.runners.Parameterized.Parameters;
 
 import com.xceptance.neodymium.NeodymiumRunner;
-import com.xceptance.neodymium.testdata.GenericFileReader;
 
 @RunWith(NeodymiumRunner.class)
 public class TestParameter
@@ -18,8 +17,9 @@ public class TestParameter
     {
         // return JsonFileReader.readFile("src/test/resources/test_data.json");
         // return CsvFileReader.readFile("src/test/resources/test_data.csv");
-        return GenericFileReader.readFile();
+        // return GenericFileReader.readFile();
         // return XmlFileReader.readFile("test_data.xml");
+        return null;
     }
 
     @Parameter(0)
