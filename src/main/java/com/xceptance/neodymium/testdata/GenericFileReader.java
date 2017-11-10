8af444a7b24cde6756a82c0c44d073cec81c254c
Marcus Kaufmann <m.kaufmann@xceptance.net>
2017-11-10 16:04:45 +0100

    added todos

--- src/main/java/com/xceptance/neodymium/testdata/GenericFileReader.java
+++ src/main/java/com/xceptance/neodymium/testdata/GenericFileReader.java
@@ -30,6 +30,8 @@ public class GenericFileReader
             {
                 return XmlFileReader.readFile(pathToFile.toFile());
             }
+
+            // TODO: property file reader
         }
 
         throw new RuntimeException("No data file found for class: " + callerClassName);
@@ -65,7 +67,7 @@ public class GenericFileReader
 
         String[] filetype = new String[]
             {
-                ".csv", ".json", ".xml"
+                ".csv", ".json", ".xml" // TODO: add properties once the file reader is created
             };
 
         String testClassPath = String.join("/", qualifier);
