95ef738ceaf1a8299a6bf627d2518d0ca8fb9b24
Marcus Kaufmann <m.kaufmann@xceptance.net>
2017-11-23 14:51:16 +0100

    added NeodymiumCucumberRunner

---
+++ src/main/java/com/xceptance/neodymium/NeodymiumCucumberRunner.java
@@ -0,0 +1,24 @@
+package com.xceptance.neodymium;
+
+import java.io.IOException;
+
+import org.junit.runner.notification.RunNotifier;
+import org.junit.runners.model.InitializationError;
+
+import cucumber.api.junit.Cucumber;
+
+public class NeodymiumCucumberRunner extends Cucumber
+{
+    public NeodymiumCucumberRunner(Class<?> clazz) throws InitializationError, IOException
+    {
+        super(clazz);
+    }
+
+    @Override
+    public void run(RunNotifier notifier)
+    {
+        // we add our own run listener in order to attach screenshots taken by Selenide to the Allure report
+        notifier.addListener(new NeodymiumRunListener());
+        super.run(notifier);
+    }
+}
