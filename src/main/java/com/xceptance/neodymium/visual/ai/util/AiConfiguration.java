1836c19a86ae6787cfcfeba5f16ff11f3dfbd0e4
Marcel Pfotenhauer <m.pfotenhauer@xceptance.net>
2017-12-20 14:04:06 +0100

    renamed properties

--- src/main/java/com/xceptance/neodymium/visual/ai/util/AiConfiguration.java
+++ src/main/java/com/xceptance/neodymium/visual/ai/util/AiConfiguration.java
@@ -10,62 +10,62 @@ import org.aeonbits.owner.Config.Sources;
 public interface AiConfiguration extends Config
 {
 
-    @Key("com.xceptance.xlt.ai.enabled")
+    @Key("neodymium.ai.enabled")
     public boolean enabled();
 
-    @Key("com.xceptance.xlt.ai.ID")
+    @Key("neodymium.ai.ID")
     @DefaultValue("all")
     public String id();
 
-    @Key("com.xceptance.xlt.ai.resultDirectory")
+    @Key("neodymium.ai.resultDirectory")
     @DefaultValue("results/ai")
     public String resultDirectory();
 
-    @Key("com.xceptance.xlt.ai.TESTCASE_BOUND")
+    @Key("neodymium.ai.TESTCASE_BOUND")
     @DefaultValue("true")
     public boolean testCaseBound();
 
-    @Key("com.xceptance.xlt.ai.TESTCASE_NAME")
+    @Key("neodymium.ai.TESTCASE_NAME")
     @DefaultValue("unbound")
     public String testCaseName();
 
-    @Key("com.xceptance.xlt.ai.TRAINING")
+    @Key("neodymium.ai.TRAINING")
     @DefaultValue("true")
     public boolean trainingMode();
 
-    @Key("com.xceptance.xlt.ai.WAITINGTIME")
+    @Key("neodymium.ai.WAITINGTIME")
     @DefaultValue("1000")
     public int waitingTime();
 
-    @Key("com.xceptance.xlt.ai.IMAGE_HEIGHT")
+    @Key("neodymium.ai.IMAGE_HEIGHT")
     @DefaultValue("800")
     public int imageHeight();
 
-    @Key("com.xceptance.xlt.ai.IMAGE_WIDTH")
+    @Key("neodymium.ai.IMAGE_WIDTH")
     @DefaultValue("600")
     public int imageWidth();
 
-    @Key("com.xceptance.xlt.ai.FORMAT")
+    @Key("neodymium.ai.FORMAT")
     @DefaultValue("png")
     public String imageFormat();
 
-    @Key("com.xceptance.xlt.ai.USE_ORIGINAL_SIZE")
+    @Key("neodymium.ai.USE_ORIGINAL_SIZE")
     @DefaultValue("true")
     public boolean useOriginalSize();
 
-    @Key("com.xceptance.xlt.ai.USE_COLOR_FOR_COMPARISON")
+    @Key("neodymium.ai.USE_COLOR_FOR_COMPARISON")
     @DefaultValue("false")
     public boolean useColorForComparison();
 
-    @Key("com.xceptance.xlt.ai.LEARNING_RATE")
+    @Key("neodymium.ai.LEARNING_RATE")
     @DefaultValue("0.2")
     public double learningRate();
 
-    @Key("com.xceptance.xlt.ai.INTENDED_PERCENTAGE_MATCH")
+    @Key("neodymium.ai.INTENDED_PERCENTAGE_MATCH")
     @DefaultValue("0.80")
     public double percentageMatch();
 
-    @Key("com.xceptance.xlt.ai.PERCENTAGE_DIFFERENCE")
+    @Key("neodymium.ai.PERCENTAGE_DIFFERENCE")
     @DefaultValue("0.1")
     public double percentageDifference();
 
--- src/main/java/com/xceptance/neodymium/visual/image/util/VisualAssertConfiguration.java
+++ src/main/java/com/xceptance/neodymium/visual/image/util/VisualAssertConfiguration.java
@@ -10,68 +10,68 @@ import org.aeonbits.owner.Config.Sources;
 public interface VisualAssertConfiguration extends Config
 {
 
-    @Key("com.xceptance.xlt.visualassertion.enabled")
+    @Key("neodymium.visualassertion.enabled")
     public boolean enabled();
 
-    @Key("com.xceptance.xlt.visualassertion.ID")
+    @Key("neodymium.visualassertion.ID")
     @DefaultValue("all")
     public String id();
 
-    @Key("com.xceptance.xlt.visualassertion.resultDirectory")
+    @Key("neodymium.visualassertion.resultDirectory")
     @DefaultValue("results/visualassertion")
     public String resultDirectory();
 
-    @Key("com.xceptance.xlt.visualassertion.waitingTime")
+    @Key("neodymium.visualassertion.waitingTime")
     @DefaultValue("300")
     public int waitingTime();
 
-    @Key("com.xceptance.xlt.visualassertion.mark.blocksize.x")
+    @Key("neodymium.visualassertion.mark.blocksize.x")
     @DefaultValue("10")
     public int markBlocksizeX();
 
-    @Key("com.xceptance.xlt.visualassertion.mark.blocksize.y")
+    @Key("neodymium.visualassertion.mark.blocksize.y")
     @DefaultValue("10")
     public int markBlocksizeY();
 
     // "box" or "marker"
-    @Key("com.xceptance.xlt.visualassertion.mark.type")
+    @Key("neodymium.visualassertion.mark.type")
     @DefaultValue("box")
     public String markType();
 
-    @Key("com.xceptance.xlt.visualassertion.fuzzy.blocksize.xy")
+    @Key("neodymium.visualassertion.fuzzy.blocksize.xy")
     @DefaultValue("10")
     public int fuzzyBlocksizeXY();
 
-    @Key("com.xceptance.xlt.visualassertion.tolerance.colors")
+    @Key("neodymium.visualassertion.tolerance.colors")
     @DefaultValue("0.1")
     public double toleranceColors();
 
-    @Key("com.xceptance.xlt.visualassertion.tolerance.pixels")
+    @Key("neodymium.visualassertion.tolerance.pixels")
     @DefaultValue("0.2")
     public double tolerancePixels();
 
-    @Key("com.xceptance.xlt.visualassertion.trainingsMode")
+    @Key("neodymium.visualassertion.trainingsMode")
     @DefaultValue("false")
     public boolean trainingMode();
 
-    @Key("com.xceptance.xlt.visualassertion.mask.close")
+    @Key("neodymium.visualassertion.mask.close")
     @DefaultValue("false")
     public boolean maskClose();
 
-    @Key("com.xceptance.xlt.visualassertion.mask.close.width")
+    @Key("neodymium.visualassertion.mask.close.width")
     @DefaultValue("5")
     public int maskCloseWidth();
 
-    @Key("com.xceptance.xlt.visualassertion.mask.close.height")
+    @Key("neodymium.visualassertion.mask.close.height")
     @DefaultValue("5")
     public int maskCloseHeight();
 
-    @Key("com.xceptance.xlt.visualassertion.onFailure.createDifferenceImage")
+    @Key("neodymium.visualassertion.onFailure.createDifferenceImage")
     @DefaultValue("true")
     public boolean createDifferenceImage();
 
     // "FUZZY" or "COLORFUZZY" or "EXACT"
-    @Key("com.xceptance.xlt.visualassertion.algorithm")
+    @Key("neodymium.visualassertion.algorithm")
     @DefaultValue("FUZZY")
     public String algorithm();
 
