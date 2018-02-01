1b05e244d39a420e9c37ce4f09e62498cd61eb54
Marcel Pfotenhauer <m.pfotenhauer@xceptance.net>
2018-02-01 14:26:22 +0100

    added ObjectMapper support to DataUtils

--- src/main/java/com/xceptance/neodymium/util/DataUtils.java
+++ src/main/java/com/xceptance/neodymium/util/DataUtils.java
@@ -5,6 +5,9 @@ import java.util.UUID;
 import org.apache.commons.lang3.ArrayUtils;
 import org.apache.commons.text.RandomStringGenerator;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 public class DataUtils
 {
     /**
@@ -46,5 +49,18 @@ public class DataUtils
         ArrayUtils.shuffle(all);
 
         return new String(all);
+    }
+
+    /**
+     * Returns data for the data type requested
+     * 
+     * @return
+     */
+    public static <T> T get(final Class<T> clazz)
+    {
+        // just use what we have and ignore if we have more, so we can build up different objects
+        // with just the data that fits
+        final ObjectMapper m = new ObjectMapper().configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        return m.convertValue(Context.get().data, clazz);
     }
 }
