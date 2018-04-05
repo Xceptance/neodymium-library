e81feb50a830d60b7709438f267ef7ea970c9886
Marcus Kaufmann <m.kaufmann@xceptance.net>
2018-04-05 19:04:41 +0200

    added tests

--- src/test/java/com/xceptance/neodymium/tests/NeodymiumBrowserRunnerTest.java
+++ src/test/java/com/xceptance/neodymium/tests/NeodymiumBrowserRunnerTest.java
@@ -1,15 +1,31 @@
 package com.xceptance.neodymium.tests;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.nio.file.Files;
+import java.text.MessageFormat;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+import org.junit.AfterClass;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.JUnitCore;
 import org.junit.runner.Result;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
+import com.google.common.base.Joiner;
+import com.xceptance.neodymium.multibrowser.configuration.BrowserConfiguration;
+import com.xceptance.neodymium.multibrowser.configuration.MultibrowserConfiguration;
 import com.xceptance.neodymium.testclasses.browser.EmptyBrowser;
 import com.xceptance.neodymium.testclasses.browser.NoBrowserTag;
 
 public class NeodymiumBrowserRunnerTest
 {
+    static List<File> tempFiles = new LinkedList<>();
+
     @Test
     public void testEmptyBrowserTag()
     {
@@ -30,5 +46,67 @@ public class NeodymiumBrowserRunnerTest
         Assert.assertEquals(0, result.getIgnoreCount());
         Assert.assertEquals(1, result.getFailureCount());
         Assert.assertEquals("Can not find browser configuration with tag: ", result.getFailures().get(0).getMessage());
+    }
+
+    @Test
+    public void testMultibrowserConfiguration() throws Throwable
+    {
+        Map<String, String> properties = new HashMap<>();
+        properties.put("browserprofile.test.name", "a test browser");
+        properties.put("browserprofile.test.browser", "chrome");
+        properties.put("browserprofile.test.browserResolution", "1024x768");
+
+        File tempConfigFile = File.createTempFile("browser", "", new File("./config/"));
+        tempFiles.add(tempConfigFile);
+        writeMapToPropertiesFile(properties, tempConfigFile);
+
+        MultibrowserConfiguration multibrowserConfiguration = MultibrowserConfiguration.getInstance(tempConfigFile.getPath());
+        Map<String, BrowserConfiguration> browserProfiles = multibrowserConfiguration.getBrowserProfiles();
+        Assert.assertEquals(1, browserProfiles.size());
+        BrowserConfiguration testProfile = browserProfiles.get("test");
+        Assert.assertNotNull(testProfile);
+        Assert.assertEquals("test", testProfile.getConfigTag());
+        Assert.assertEquals(1024, testProfile.getBrowserWidth());
+        Assert.assertEquals(768, testProfile.getBrowserHeight());
+        Assert.assertEquals("a test browser", testProfile.getName());
+        Assert.assertEquals(null, testProfile.getTestEnvironment());
+        DesiredCapabilities testCapabilities = testProfile.getCapabilities();
+        Assert.assertEquals("chrome", testCapabilities.getBrowserName());
+    }
+
+    public void writeMapToPropertiesFile(Map<String, String> map, File file)
+    {
+        try
+        {
+            String join = Joiner.on("\r\n").withKeyValueSeparator("=").join(map);
+
+            FileOutputStream outputStream = new FileOutputStream(file);
+            outputStream.write(join.getBytes());
+            outputStream.close();
+        }
+        catch (Exception e)
+        {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @AfterClass
+    public static void cleanUp()
+    {
+        for (File f : tempFiles)
+        {
+            if (f.exists())
+            {
+                try
+                {
+                    Files.delete(f.toPath());
+                }
+                catch (Exception e)
+                {
+                    System.out.println(MessageFormat.format("couldn''t delete temporary file: ''{0}'' caused by {1}", f.getAbsolutePath(),
+                                                            e));
+                }
+            }
+        }
     }
 }
