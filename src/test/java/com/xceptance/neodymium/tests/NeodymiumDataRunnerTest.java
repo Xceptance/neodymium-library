abaff9c1becc67a9f60f12142226b2fe1490d235
Marcus Kaufmann <m.kaufmann@xceptance.net>
2018-04-10 16:25:00 +0200

    data set tests

--- src/test/java/com/xceptance/neodymium/tests/NeodymiumDataRunnerTest.java
+++ src/test/java/com/xceptance/neodymium/tests/NeodymiumDataRunnerTest.java
@@ -1,5 +1,6 @@
 package com.xceptance.neodymium.tests;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.JUnitCore;
 import org.junit.runner.Result;
@@ -8,9 +9,24 @@ import com.xceptance.neodymium.testclasses.data.pkg.csv.CanReadPackageDataCSV;
 import com.xceptance.neodymium.testclasses.data.pkg.json.CanReadPackageDataJson;
 import com.xceptance.neodymium.testclasses.data.pkg.properties.CanReadPackageDataProperties;
 import com.xceptance.neodymium.testclasses.data.pkg.xml.CanReadPackageDataXML;
+import com.xceptance.neodymium.testclasses.data.set.csv.CanReadDataSetCSV;
+import com.xceptance.neodymium.testclasses.data.set.json.CanReadDataSetJson;
+import com.xceptance.neodymium.testclasses.data.set.properties.CanReadDataSetProperties;
+import com.xceptance.neodymium.testclasses.data.set.xml.CanReadDataSetXML;
+import com.xceptance.neodymium.util.Context;
 
 public class NeodymiumDataRunnerTest extends NeodymiumTest
 {
+
+    @Before
+    public void beforeTest()
+    {
+        // we need to clear the context before test starts because all of the tests are running in the same thread and
+        // the context holds data based on the current thread. since data sets and package data is also stored in the
+        // context this data would never be updated
+        Context.clearThreadContext();
+    }
+
     @Test
     public void testCanReadPackageDataCSV()
     {
@@ -36,6 +52,34 @@ public class NeodymiumDataRunnerTest extends NeodymiumTest
     public void testCanReadPackageDataXML()
     {
         Result result = JUnitCore.runClasses(CanReadPackageDataXML.class);
+        checkPass(result, 1, 0, 0);
+    }
+
+    @Test
+    public void testCanReadDataSetCSV()
+    {
+        Result result = JUnitCore.runClasses(CanReadDataSetCSV.class);
+        checkPass(result, 1, 0, 0);
+    }
+
+    @Test
+    public void testCanReadDataSetJson()
+    {
+        Result result = JUnitCore.runClasses(CanReadDataSetJson.class);
+        checkPass(result, 1, 0, 0);
+    }
+
+    @Test
+    public void testCanReadDataSetProperties()
+    {
+        Result result = JUnitCore.runClasses(CanReadDataSetProperties.class);
+        checkPass(result, 1, 0, 0);
+    }
+
+    @Test
+    public void testCanReadDataSetXML()
+    {
+        Result result = JUnitCore.runClasses(CanReadDataSetXML.class);
         checkPass(result, 1, 0, 0);
     }
 }
