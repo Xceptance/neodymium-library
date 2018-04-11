260e4ab30b54a31745f7ef1102423d4634e17cf6
Marcus Kaufmann <m.kaufmann@xceptance.net>
2018-04-11 11:28:41 +0200

    multiplication tests

--- src/test/java/com/xceptance/neodymium/tests/MultiplicationTest.java
+++ src/test/java/com/xceptance/neodymium/tests/MultiplicationTest.java
@@ -5,6 +5,10 @@ import org.junit.Test;
 import com.xceptance.neodymium.testclasses.multiplication.TwoMethods;
 import com.xceptance.neodymium.testclasses.multiplication.TwoMethodsAndOneIgnored;
 import com.xceptance.neodymium.testclasses.multiplication.TwoMethodsOneIgnored;
+import com.xceptance.neodymium.testclasses.multiplication.dataset.OneDataSetOneMethod;
+import com.xceptance.neodymium.testclasses.multiplication.dataset.OneDataSetTwoMethods;
+import com.xceptance.neodymium.testclasses.multiplication.dataset.TwoDataSetsOneMethod;
+import com.xceptance.neodymium.testclasses.multiplication.dataset.TwoDataSetsTwoMethods;
 
 public class MultiplicationTest extends NeodymiumTest
 {
@@ -36,6 +40,53 @@ public class MultiplicationTest extends NeodymiumTest
                 "first", "second"
             };
         checkDescription(TwoMethodsOneIgnored.class, expected);
+    }
+
+    @Test
+    public void testOneDataSetOneMethod() throws Throwable
+    {
+        String[] expected = new String[]
+            {
+                "first :: Data set 1 / 1"
+            };
+        checkDescription(OneDataSetOneMethod.class, expected);
+    }
+
+    @Test
+    public void testOneDataSetTwoMethods() throws Throwable
+    {
+        String[] expected = new String[]
+            {
+                "first :: Data set 1 / 1", //
+                "second :: Data set 1 / 1"
+            };
+        checkDescription(OneDataSetTwoMethods.class, expected);
+    }
+
+    @Test
+    public void testTwoDataSetsOneMethod() throws Throwable
+    {
+        String[] expected = new String[]
+            {
+                "first :: Data set 1 / 2", //
+                "first :: Data set 2 / 2"
+            };
+        checkDescription(TwoDataSetsOneMethod.class, expected);
+
+    }
+
+    @Test
+    public void testTwoDataSetsTwoMethods() throws Throwable
+    {
+        String[] expected = new String[]
+            {
+                "first :: Data set 1 / 2", //
+                "first :: Data set 2 / 2", //
+                "second :: Data set 1 / 2", //
+                "second :: Data set 2 / 2", //
+            };
+        checkDescription(TwoDataSetsTwoMethods.class, expected);
+
     }
 
 }
