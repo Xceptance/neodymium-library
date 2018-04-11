a5934a92f6a74a604f96c10c8c402e41c2c1a109
Marcus Kaufmann <m.kaufmann@xceptance.net>
2018-04-11 13:10:59 +0200

    method tests

--- src/test/java/com/xceptance/neodymium/tests/NeodymiumMethodRunnerTest.java
+++ src/test/java/com/xceptance/neodymium/tests/NeodymiumMethodRunnerTest.java
@@ -1,9 +1,11 @@
 package com.xceptance.neodymium.tests;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.JUnitCore;
 import org.junit.runner.Result;
 
+import com.xceptance.neodymium.testclasses.method.BeforeAndAfter;
 import com.xceptance.neodymium.testclasses.method.ConstructorThrowsCheckedException;
 import com.xceptance.neodymium.testclasses.method.ConstructorThrowsError;
 import com.xceptance.neodymium.testclasses.method.FailingMethod;
@@ -59,6 +61,24 @@ public class NeodymiumMethodRunnerTest extends NeodymiumTest
         // test that the test fails if the constructor throws an error
         Result result = JUnitCore.runClasses(ConstructorThrowsError.class);
         checkFail(result, 0, 0, 1, "java.lang.reflect.InvocationTargetException");
+    }
+
+    @Test
+    public void testBeforeAndAfter()
+    {
+        // test @Before, @After, @BeforeClass, @AfterClass
+        Result result = JUnitCore.runClasses(BeforeAndAfter.class);
+        checkPass(result, 1, 0, 0);
+        String[] expected = new String[]
+            {
+                "beforeClass", //
+                "beforeMethod", //
+                "first", //
+                "afterMethod", //
+                "afterClass"
+            };
+        String[] trace = BeforeAndAfter.TRACE.toArray(new String[0]);
+        Assert.assertArrayEquals(expected, trace);
     }
 
 }
