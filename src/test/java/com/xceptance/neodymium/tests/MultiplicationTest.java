b17e203bece6e8dbe1dd25f9e467a235a5685475
Marcus Kaufmann <m.kaufmann@xceptance.net>
2018-04-11 16:30:15 +0200

    paramter multiplication

--- src/test/java/com/xceptance/neodymium/tests/MultiplicationTest.java
+++ src/test/java/com/xceptance/neodymium/tests/MultiplicationTest.java
@@ -9,10 +9,18 @@ import com.xceptance.neodymium.testclasses.multiplication.dataset.OneDataSetOneM
 import com.xceptance.neodymium.testclasses.multiplication.dataset.OneDataSetTwoMethods;
 import com.xceptance.neodymium.testclasses.multiplication.dataset.TwoDataSetsOneMethod;
 import com.xceptance.neodymium.testclasses.multiplication.dataset.TwoDataSetsTwoMethods;
+import com.xceptance.neodymium.testclasses.multiplication.parameter.OneParameterSetOneMethod;
+import com.xceptance.neodymium.testclasses.multiplication.parameter.OneParameterSetTwoMethods;
+import com.xceptance.neodymium.testclasses.multiplication.parameter.TwoParameterSetsOneMethod;
+import com.xceptance.neodymium.testclasses.multiplication.parameter.TwoParameterSetsTwoMethods;
 import com.xceptance.neodymium.testclasses.multiplication.pkgdata.PackageDataDoNotAffectMethodMultiplication;
 
 public class MultiplicationTest extends NeodymiumTest
 {
+    //////////////////
+    // Methods only //
+    //////////////////
+
     @Test
     public void testTwoMethods() throws Throwable
     {
@@ -46,6 +54,10 @@ public class MultiplicationTest extends NeodymiumTest
         checkDescription(TwoMethodsOneIgnored.class, expected);
     }
 
+    //////////////////////////////
+    // Methods and package data //
+    //////////////////////////////
+
     @Test
     public void testPackageDataDoNotAffectMethodMultiplication() throws Throwable
     {
@@ -57,6 +69,10 @@ public class MultiplicationTest extends NeodymiumTest
         checkDescription(PackageDataDoNotAffectMethodMultiplication.class, expected);
 
     }
+
+    ///////////////////////////
+    // Methods and data sets //
+    ///////////////////////////
 
     @Test
     public void testOneDataSetOneMethod() throws Throwable
@@ -106,6 +122,55 @@ public class MultiplicationTest extends NeodymiumTest
                 "second :: Data set 2 / 2", //
             };
         checkDescription(TwoDataSetsTwoMethods.class, expected);
+    }
+
+    ///////////////////////////
+    // Methods and parameter //
+    ///////////////////////////
+
+    @Test
+    public void testOneParameterSetOneMethod() throws Throwable
+    {
+        String[] expected = new String[]
+            {
+                "first :: [0]"
+            };
+        checkDescription(OneParameterSetOneMethod.class, expected);
+    }
+
+    @Test
+    public void testOneParameterSetTwoMethods() throws Throwable
+    {
+        String[] expected = new String[]
+            {
+                "first :: [0]", //
+                "second :: [0]"
+            };
+        checkDescription(OneParameterSetTwoMethods.class, expected);
+    }
+
+    @Test
+    public void testTwoParameterSetsOneMethod() throws Throwable
+    {
+        String[] expected = new String[]
+            {
+                "first :: [0]", //
+                "first :: [1]"
+            };
+        checkDescription(TwoParameterSetsOneMethod.class, expected);
+    }
+
+    @Test
+    public void testTwoParameterSetsTwoMethods() throws Throwable
+    {
+        String[] expected = new String[]
+            {
+                "first :: [0]", //
+                "first :: [1]", //
+                "second :: [0]", //
+                "second :: [1]"
+            };
+        checkDescription(TwoParameterSetsTwoMethods.class, expected);
     }
 
 }
