6a967461d055be6c9b7b6ea8f5c09dc59159f73a
Marcus Kaufmann <m.kaufmann@xceptance.net>
2018-04-24 15:24:56 +0200

    parameter vector

---
+++ src/main/java/com/xceptance/neodymium/module/vector/ParameterVector.java
@@ -0,0 +1,41 @@
+package com.xceptance.neodymium.module.vector;
+
+public class ParameterVector implements RunVector
+{
+
+    @Override
+    public void beforeMethod()
+    {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void afterMethod()
+    {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public String getTestName()
+    {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public int vectorHashCode()
+    {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public void setTestClassInstance(Object testClassInstance)
+    {
+        // TODO Auto-generated method stub
+
+    }
+
+}
---
+++ src/main/java/com/xceptance/neodymium/module/vector/ParameterVectorBuilder.java
@@ -0,0 +1,25 @@
+package com.xceptance.neodymium.module.vector;
+
+import java.util.List;
+
+import org.junit.runners.model.FrameworkMethod;
+import org.junit.runners.model.TestClass;
+
+public class ParameterVectorBuilder implements RunVectorBuilder
+{
+
+    @Override
+    public void create(TestClass testClass, FrameworkMethod frameworkMethod)
+    {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public List<RunVector> buildRunVectors()
+    {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
