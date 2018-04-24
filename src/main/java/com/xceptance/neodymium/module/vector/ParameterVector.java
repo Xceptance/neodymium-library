80b80a1bbaceceffc1b533d683c72e94374b8db5
Marcus Kaufmann <m.kaufmann@xceptance.net>
2018-04-24 18:52:04 +0200

    parameter vector

--- src/main/java/com/xceptance/neodymium/module/vector/ParameterVector.java
+++ src/main/java/com/xceptance/neodymium/module/vector/ParameterVector.java
@@ -1,20 +1,46 @@
 package com.xceptance.neodymium.module.vector;
 
+import java.lang.reflect.Field;
+import java.util.List;
+
+import org.junit.runners.Parameterized.Parameter;
+import org.junit.runners.model.FrameworkField;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class ParameterVector implements RunVector
 {
+    public static Logger LOGGER = LoggerFactory.getLogger(ParameterVector.class);
+
+    private Object testClassInstance;
+
+    private Object[] parameter;
+
+    private List<FrameworkField> parameterFrameworkFields;
+
+    public ParameterVector(List<FrameworkField> parameterFrameworkFields, Object[] parameter)
+    {
+        this.parameterFrameworkFields = parameterFrameworkFields;
+        this.parameter = parameter;
+    }
 
     @Override
     public void beforeMethod()
     {
-        // TODO Auto-generated method stub
-
+        try
+        {
+            injectTestParameter();
+        }
+        catch (Exception e)
+        {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     public void afterMethod()
     {
-        // TODO Auto-generated method stub
-
+        // nothing
     }
 
     @Override
@@ -34,8 +60,87 @@ public class ParameterVector implements RunVector
     @Override
     public void setTestClassInstance(Object testClassInstance)
     {
-        // TODO Auto-generated method stub
+        this.testClassInstance = testClassInstance;
 
     }
 
+    private void injectTestParameter() throws Exception
+    {
+        if (parameterFrameworkFields.size() != parameter.length)
+        {
+            throw new Exception("Number of parameters (" + parameter.length + ") and " + //
+                                "fields (" + parameterFrameworkFields.size() + ") " + //
+                                "annotated with @Parameter must match!");
+        }
+
+        for (FrameworkField parameterFrameworkField : parameterFrameworkFields)
+        {
+            Field field = parameterFrameworkField.getField();
+            int parameterIndex = field.getAnnotation(Parameter.class).value();
+
+            LOGGER.debug("Set parameter \"" + parameterFrameworkField.getName() + "\" to \"" + parameter[parameterIndex] + "\"");
+            setField(field, parameter[parameterIndex]);
+        }
+    }
+
+    private void setField(Field field, Object value)
+    {
+        Class<?> fieldType = field.getType();
+        Class<?> valueType;
+        if (value == null)
+        {
+            valueType = Void.class;
+        }
+        else
+        {
+            valueType = value.getClass();
+        }
+
+        // try to convert String values to appropriate target types
+        if (valueType == String.class)
+        {
+            try
+            {
+                if (fieldType == int.class || fieldType == Integer.class)
+                {
+                    value = Integer.valueOf((String) value);
+                }
+                else if (fieldType == long.class || fieldType == Long.class)
+                {
+                    value = Long.valueOf((String) value);
+                }
+                else if (fieldType == double.class || fieldType == Double.class)
+                {
+                    value = Double.valueOf((String) value);
+                }
+                else if (fieldType == float.class || fieldType == Float.class)
+                {
+                    value = Float.valueOf((String) value);
+                }
+                else if (fieldType == boolean.class || fieldType == Boolean.class)
+                {
+                    value = Boolean.valueOf((String) value);
+                }
+            }
+            catch (Exception e)
+            {
+                throw new RuntimeException("An error occured during conversion of input string \"" + (String) value + "\" to type " +
+                                           fieldType.getName() + " for field \"" + field.getName() + "\"", e);
+            }
+        }
+
+        try
+        {
+            field.set(testClassInstance, value);
+        }
+        catch (IllegalArgumentException e)
+        {
+            throw new RuntimeException("Could not set parameter of type " + valueType + " to field \"" + field.getName() + "\" of type " +
+                                       fieldType + ". Value: " + value);
+        }
+        catch (IllegalAccessException e)
+        {
+            throw new RuntimeException("Could not set parameter due to it is not public or it is final");
+        }
+    }
 }
--- src/main/java/com/xceptance/neodymium/module/vector/ParameterVectorBuilder.java
+++ src/main/java/com/xceptance/neodymium/module/vector/ParameterVectorBuilder.java
@@ -1,25 +1,94 @@
 package com.xceptance.neodymium.module.vector;
 
+import java.text.MessageFormat;
+import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
 
+import org.junit.runners.Parameterized.Parameter;
+import org.junit.runners.Parameterized.Parameters;
+import org.junit.runners.model.FrameworkField;
 import org.junit.runners.model.FrameworkMethod;
 import org.junit.runners.model.TestClass;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class ParameterVectorBuilder implements RunVectorBuilder
 {
+    public static Logger LOGGER = LoggerFactory.getLogger(ParameterVectorBuilder.class);
+
+    private List<FrameworkField> parameterFrameworkFields;
+
+    private Iterable<Object> parameter = null;
 
     @Override
     public void create(TestClass testClass, FrameworkMethod frameworkMethod)
     {
-        // TODO Auto-generated method stub
+        List<FrameworkMethod> parametersMethods = testClass.getAnnotatedMethods(Parameters.class);
+
+        for (FrameworkMethod parametersMethod : parametersMethods)
+        {
+            if (parametersMethod.isPublic() && parametersMethod.isStatic())
+            {
+                // take the first public static method. invoke it and use the result as parameter
+                try
+                {
+                    Object parametersResult = parametersMethod.invokeExplosively(null);
+                    if (parametersResult instanceof Iterable)
+                    {
+                        parameter = (Iterable<Object>) parametersResult;
+                    }
+                    else if (parametersResult instanceof Object[])
+                    {
+                        parameter = Arrays.asList((Object[]) parametersResult);
+                    }
+                    else
+                    {
+                        String msg = MessageFormat.format("{0}.{1}() must return an Iterable of arrays.",
+                                                          testClass.getJavaClass().getName(), parametersMethod.getName());
+                        throw new Exception(msg);
+                    }
+                    break;
+                }
+                catch (Throwable e)
+                {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
+
+        parameterFrameworkFields = testClass.getAnnotatedFields(Parameter.class);
+        LOGGER.debug("Found " + parameterFrameworkFields.size() + " parameter fields");
 
     }
 
     @Override
     public List<RunVector> buildRunVectors()
     {
-        // TODO Auto-generated method stub
-        return null;
+        List<RunVector> r = new LinkedList<>();
+
+        if (parameter != null)
+        {
+            for (Object para : parameter)
+            {
+                Object[] p;
+                if (para instanceof Object[])
+                {
+                    p = (Object[]) para;
+                }
+                else
+                {
+                    p = new Object[]
+                        {
+                            para
+                        };
+                }
+
+                r.add(new ParameterVector(parameterFrameworkFields, p));
+            }
+        }
+
+        return r;
     }
 
 }
