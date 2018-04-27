d8051bc0cb86e68eac9ef2e69ba28edb9d29ca60
Marcus Kaufmann <m.kaufmann@xceptance.net>
2018-04-27 18:25:06 +0200

    tests

---
+++ src/test/java/com/xceptance/neodymium/tests/refactor/Blubb.java
@@ -0,0 +1,22 @@
+package com.xceptance.neodymium.tests.refactor;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import com.xceptance.neodymium.NeodymiumStatementRunner2;
+import com.xceptance.neodymium.multibrowser.Browser;
+
+@RunWith(NeodymiumStatementRunner2.class)
+@Browser(
+    {
+        "A", "B"
+    })
+public class Blubb
+{
+
+    @Test
+    public void testName() throws Exception
+    {
+
+    }
+}
--- src/test/java/com/xceptance/neodymium/tests/refactor/IAmStupidToo.java
+++ src/test/java/com/xceptance/neodymium/tests/refactor/IAmStupidToo.java
@@ -1,23 +1,54 @@
 package com.xceptance.neodymium.tests.refactor;
 
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import com.xceptance.neodymium.NeodymiumRunner;
+import com.xceptance.neodymium.NeodymiumStatementRunner2;
 import com.xceptance.neodymium.multibrowser.Browser;
 
 //@RunOrder({BrowserVector.class, TestdataVector.class, PrarameterVector.class})
-@RunWith(NeodymiumRunner.class)
+@RunWith(NeodymiumStatementRunner2.class)
 @Browser(
     {
         "A", "B"
     })
 public class IAmStupidToo
 {
+    @BeforeClass
+    public static void beforeClass()
+    {
+        System.out.println("IAmStupidToo.beforeClass()");
+    }
+
+    @AfterClass
+    public static void afterClass()
+    {
+        System.out.println("IAmStupidToo.afterClass()");
+    }
+
+    @Before
+    public void before()
+    {
+        System.out.println("IAmStupidToo.before()");
+    }
+
+    @After
+    public void after()
+    {
+        System.out.println("IAmStupidToo.after()");
+    }
+
     @Test
-    @Browser({})
+    @Browser(
+        {
+            "C", "D"
+        })
     public void test7_EMPTY() throws Exception
     {
-
+        System.out.println("bla");
     }
 }
