d42131ec7d2c991992a89a44b09353dcbfc94483
Marcus Kaufmann <m.kaufmann@xceptance.net>
2018-05-04 15:05:29 +0200

    dataset and suppress data set handling

--- src/main/java/com/xceptance/neodymium/module/statement/testdata/TestdataStatement.java
+++ src/main/java/com/xceptance/neodymium/module/statement/testdata/TestdataStatement.java
@@ -3,6 +3,7 @@ package com.xceptance.neodymium.module.statement.testdata;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.text.MessageFormat;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
@@ -22,6 +23,8 @@ import com.xceptance.neodymium.util.Context;
 
 public class TestdataStatement extends StatementBuilder
 {
+    private static final String TEST_ID = "testId";
+
     public static Logger LOGGER = LoggerFactory.getLogger(TestdataStatement.class);
 
     private Statement next;
@@ -106,38 +109,172 @@ public class TestdataStatement extends StatementBuilder
             // we couldn't find any data sets
         }
 
-        // check first the method an then the class for an Testdata annotation
-        // if there is any fixed dataset set then modidy iteration list
-        Testdata annotation = method.getAnnotation(Testdata.class);
-        if (annotation == null)
+        return processOverrides(testClass, method, iterations);
+    }
+
+    private List<Object> processOverrides(TestClass testClass, FrameworkMethod method, List<Object> iterations)
+    {
+        SuppressDataSets methodSuppress = method.getAnnotation(SuppressDataSets.class);
+        if (methodSuppress != null)
         {
-            annotation = testClass.getAnnotation(Testdata.class);
+            // the test method is marked to suppress data sets
+            return new LinkedList<>();
         }
-        if (annotation != null)
+
+        SuppressDataSets classSuppress = testClass.getAnnotation(SuppressDataSets.class);
+        List<DataSet> methodDataSetAnnotations = getMethodDataSetAnnotations(method);
+
+        if (methodDataSetAnnotations.isEmpty() && classSuppress != null)
         {
-            int fixedDataSetIndex = annotation.value();
-            // just ignore all values <= 0
-            if (fixedDataSetIndex > -1)
+            // class is marked to suppress data sets and there is no overriding DataSet on the method
+            return new LinkedList<>();
+        }
+
+        List<DataSet> dataSetAnnotations = new LinkedList<>();
+        if (classSuppress == null)
+        {
+            List<DataSet> classDataSetAnnotations = getClassDataSetAnnotations(testClass);
+
+            // at this point neither the class nor the method could have a data set supress
+            if (!methodDataSetAnnotations.isEmpty())
             {
-                if (fixedDataSetIndex > iterations.size())
-                {
-                    String msg = MessageFormat.format("Method ''{0}'' is marked to be run only with data set index {1}, but there are only {2}",
-                                                      method.getName(), fixedDataSetIndex, iterations.size());
-                    throw new IllegalArgumentException(msg);
-                }
-
-                List<Object> fixedIteration = new LinkedList<>();
-                // we define 0 to equal to run without datasets
-                if (fixedDataSetIndex > 0)
-                {
-                    fixedIteration.add(iterations.get(fixedDataSetIndex - 1));
-                }
-
-                return fixedIteration;
+                dataSetAnnotations = methodDataSetAnnotations;
+            }
+            else if (!classDataSetAnnotations.isEmpty())
+            {
+                dataSetAnnotations = classDataSetAnnotations;
             }
         }
 
-        return iterations;
+        if (dataSetAnnotations.isEmpty())
+        {
+            // so there is nothing to suppress and nothing to override. Go ahead with all data sets
+            return iterations;
+        }
+
+        List<Object> fixedIterations = new LinkedList<>();
+        for (DataSet dataSet : dataSetAnnotations)
+        {
+            int dataSetIndex = dataSet.value();
+            String dataSetId = dataSet.id();
+
+            // take dataSetId (testId) if is set
+            if (dataSetId != null && dataSetId.trim().length() > 0)
+            {
+                // search the dataset
+                boolean found = false;
+                for (Object object : iterations)
+                {
+                    Map<String, String> dataSetInstance = (Map<String, String>) object;
+                    String testId = dataSetInstance.get(TEST_ID);
+                    if (dataSetId.equals(testId))
+                    {
+                        fixedIterations.add(object);
+                        found = true;
+                        break;
+                    }
+                }
+                if (!found)
+                {
+                    String msg = MessageFormat.format("Could not find data set with test id ''{0}''", dataSetId);
+                    throw new IllegalArgumentException(msg);
+                }
+            }
+            else
+            {
+                if (dataSetIndex <= 0)
+                {
+                    // add all data sets
+                    fixedIterations.addAll(iterations);
+                }
+                else
+                {
+                    if (dataSetIndex > iterations.size())
+                    {
+                        String msg = MessageFormat.format("Method ''{0}'' is marked to be run only with data set index {1}, but there are only {2}",
+                                                          method.getName(), dataSetIndex, iterations.size());
+                        throw new IllegalArgumentException(msg);
+                    }
+                    else
+                    {
+                        fixedIterations.add(iterations.get(dataSetIndex));
+                    }
+                }
+            }
+        }
+        return fixedIterations;
+
+        // // check first the method an then the class for an Testdata annotation
+        // // if there is any fixed dataset set then modidy iteration list
+        // DataSet annotation = method.getAnnotation(DataSet.class);
+        // if (annotation == null)
+        // {
+        // annotation = testClass.getAnnotation(DataSet.class);
+        // }
+        // if (annotation != null)
+        // {
+        // int fixedDataSetIndex = annotation.value();
+        // // just ignore all values <= 0
+        // if (fixedDataSetIndex > -1)
+        // {
+        // if (fixedDataSetIndex > iterations.size())
+        // {
+        // String msg = MessageFormat.format("Method ''{0}'' is marked to be run only with data set index {1}, but there
+        // are only {2}",
+        // method.getName(), fixedDataSetIndex, iterations.size());
+        // throw new IllegalArgumentException(msg);
+        // }
+        //
+        // List<Object> fixedIteration = new LinkedList<>();
+        // // we define 0 to equal to run without datasets
+        // if (fixedDataSetIndex > 0)
+        // {
+        // fixedIteration.add(iterations.get(fixedDataSetIndex - 1));
+        // }
+        //
+        // return fixedIteration;
+        // }
+        // }
+        //
+        // return iterations;
+    }
+
+    private List<DataSet> getClassDataSetAnnotations(TestClass testClass)
+    {
+        List<DataSet> annotations = new LinkedList<>();
+
+        DataSets classDataSets = testClass.getAnnotation(DataSets.class);
+        if (classDataSets != null)
+        {
+            annotations.addAll(Arrays.asList(classDataSets.value()));
+        }
+
+        DataSet classDataSet = testClass.getAnnotation(DataSet.class);
+        if (classDataSet != null)
+        {
+            annotations.add(classDataSet);
+        }
+
+        return annotations;
+    }
+
+    private List<DataSet> getMethodDataSetAnnotations(FrameworkMethod method)
+    {
+        List<DataSet> annotations = new LinkedList<>();
+
+        DataSets classDataSets = method.getAnnotation(DataSets.class);
+        if (classDataSets != null)
+        {
+            annotations.addAll(Arrays.asList(classDataSets.value()));
+        }
+
+        DataSet classDataSet = method.getAnnotation(DataSet.class);
+        if (classDataSet != null)
+        {
+            annotations.add(classDataSet);
+        }
+
+        return annotations;
     }
 
     @Override
@@ -158,7 +295,7 @@ public class TestdataStatement extends StatementBuilder
         if (parameter.getIndex() >= 0)
         {
             // data sets and (maybe) package data
-            String testDatasetIndetifier = testData.get("ID");
+            String testDatasetIndetifier = testData.get(TEST_ID);
             if (StringUtils.isBlank(testDatasetIndetifier))
             {
                 testDatasetIndetifier = "Data set";
