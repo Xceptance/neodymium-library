62de386b569cb8e57fa8aef234fd91c47406fb68
Marcus Kaufmann <m.kaufmann@xceptance.net>
2018-05-17 17:00:46 +0200

    made browser annotation repeatable

--- src/main/java/com/xceptance/neodymium/module/statement/browser/multibrowser/Browser.java
+++ src/main/java/com/xceptance/neodymium/module/statement/browser/multibrowser/Browser.java
@@ -2,6 +2,7 @@ package com.xceptance.neodymium.module.statement.browser.multibrowser;
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Inherited;
+import java.lang.annotation.Repeatable;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
@@ -20,6 +21,7 @@ import java.lang.annotation.Target;
         ElementType.TYPE, ElementType.METHOD
     })
 @Inherited
+@Repeatable(Browsers.class)
 public @interface Browser
 {
     String[] value();
