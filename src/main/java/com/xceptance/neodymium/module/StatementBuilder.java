99a2541a214b731dca551d6588c47a00c4dfb8af
Marcus Kaufmann <m.kaufmann@xceptance.net>
2018-05-17 16:59:16 +0200

    added method to retrieve repeatable annotations from everything

--- src/main/java/com/xceptance/neodymium/module/StatementBuilder.java
+++ src/main/java/com/xceptance/neodymium/module/StatementBuilder.java
@@ -1,5 +1,11 @@
 package com.xceptance.neodymium.module;
 
+import java.lang.annotation.Annotation;
+import java.lang.annotation.Repeatable;
+import java.lang.reflect.AnnotatedElement;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.LinkedList;
 import java.util.List;
 
 import org.junit.runners.model.FrameworkMethod;
@@ -30,5 +36,45 @@ public abstract class StatementBuilder extends Statement
         {
             throw new RuntimeException(e);
         }
+    }
+
+    public static <T extends Annotation> List<T> getAnnotations(AnnotatedElement object, Class<T> annotationClass)
+    {
+        List<T> annotations = new LinkedList<>();
+        if (object == null || annotationClass == null)
+        {
+            return annotations;
+        }
+
+        // check if the annotation is repeatable
+        Repeatable repeatingAnnotation = annotationClass.getAnnotation(Repeatable.class);
+        if (repeatingAnnotation != null)
+        {
+            Annotation annotation = object.getAnnotation(repeatingAnnotation.value());
+
+            if (annotation != null)
+            {
+                try
+                {
+                    List<Object> repeatedAnnotations = Arrays.asList(((Object[]) annotation.getClass().getMethod("value")
+                                                                                           .invoke(annotation)));
+                    annotations.addAll((Collection<? extends T>) repeatedAnnotations);
+                }
+                catch (Exception e)
+                {
+                    throw new RuntimeException(e);
+                }
+            }
+            else
+            {
+                T anno = object.getAnnotation(annotationClass);
+                if (anno != null)
+                {
+                    annotations.add(anno);
+                }
+            }
+        }
+
+        return annotations;
     }
 }
