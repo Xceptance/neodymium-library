da1cbd7e4ad96a68822aa5dabeea3be6fe076fae
Marcel Pfotenhauer <m.pfotenhauer@xceptance.net>
2018-06-11 14:42:40 +0200

    moved test resources

--- src/test/java/com/xceptance/neodymium/testclasses/context/cucumbercontextclear/CucumberContextGetsCleared.java
+++ src/test/java/com/xceptance/neodymium/testclasses/context/cucumbercontextclear/CucumberContextGetsCleared.java
@@ -7,8 +7,7 @@ import com.xceptance.neodymium.NeodymiumCucumberRunner;
 import cucumber.api.CucumberOptions;
 
 @RunWith(NeodymiumCucumberRunner.class)
-@CucumberOptions(features = "src/test/java/com/xceptance/neodymium/testclasses/context/cucumbercontextclear",
-                 glue = "com/xceptance/neodymium/testclasses/context/cucumbercontextclear")
+@CucumberOptions(features = "src/test/resources/com/xceptance/neodymium/testclasses/context/cucumbercontextclear", glue = "com/xceptance/neodymium/testclasses/context/cucumbercontextclear")
 public class CucumberContextGetsCleared
 {
 }
