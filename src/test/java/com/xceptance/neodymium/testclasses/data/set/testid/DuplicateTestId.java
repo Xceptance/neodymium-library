fb673f8aa75b99b63ef31b50a14eb00eb13d662f
Marcus Kaufmann <m.kaufmann@xceptance.net>
2018-06-18 13:32:57 +0200

    extended duplicate "testId" test to use additional dataset annotation

--- src/test/java/com/xceptance/neodymium/testclasses/data/set/testid/DuplicateTestId.java
+++ src/test/java/com/xceptance/neodymium/testclasses/data/set/testid/DuplicateTestId.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import com.xceptance.neodymium.NeodymiumRunner;
+import com.xceptance.neodymium.module.statement.testdata.DataSet;
 import com.xceptance.neodymium.util.Context;
 
 @RunWith(NeodymiumRunner.class)
@@ -31,5 +32,29 @@ public class DuplicateTestId
             Assert.assertEquals("value3", Context.dataValue("testParam1"));
         }
         dataSet++;
+    }
+
+    @Test
+    @DataSet(id = "id1")
+    public void testDuplicateTestIdByAnnotation1() throws Exception
+    {
+        Assert.assertEquals("id1", Context.dataValue("testId"));
+        Assert.assertEquals("value1", Context.dataValue("testParam1"));
+    }
+
+    @Test
+    @DataSet(id = "id2")
+    public void testDuplicateTestIdByAnnotation2() throws Exception
+    {
+        Assert.assertEquals("id2", Context.dataValue("testId"));
+        Assert.assertEquals("value2", Context.dataValue("testParam1"));
+    }
+
+    @Test
+    @DataSet(3)
+    public void testDuplicateTestIdByAnnotation3() throws Exception
+    {
+        Assert.assertEquals("id1", Context.dataValue("testId"));
+        Assert.assertEquals("value3", Context.dataValue("testParam1"));
     }
 }
