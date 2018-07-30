5463e0ad2ba5886dce76e07814e830326cb1ded8
Marcel Pfotenhauer <m.pfotenhauer@xceptance.net>
2018-07-30 10:33:29 +0200

    fixed typo

--- src/main/java/com/xceptance/neodymium/module/statement/testdata/TestdataStatement.java
+++ src/main/java/com/xceptance/neodymium/module/statement/testdata/TestdataStatement.java
@@ -292,7 +292,7 @@ public class TestdataStatement extends StatementBuilder
                 testDatasetIndetifier = "Data set";
             }
 
-            // replace paranthesis because https://bugs.eclipse.org/bugs/show_bug.cgi?id=102512
+            // replace parenthesis because https://bugs.eclipse.org/bugs/show_bug.cgi?id=102512
             // "Any text in parentheses is assumed to be the name of the class in which the test is defined."
             testDatasetIndetifier = testDatasetIndetifier.replaceAll("\\(", "[").replaceAll("\\)", "]");
 
