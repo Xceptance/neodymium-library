6533ca74a8f6e82bfe8bf14d465f19e6a6a795e0
Marcus Kaufmann <m.kaufmann@xceptance.net>
2018-07-31 10:56:21 +0200

    suppress testcase error output

--- src/test/java/com/xceptance/neodymium/testclasses/cucumber/CucumberSetBrowserViaTagFail.java
+++ src/test/java/com/xceptance/neodymium/testclasses/cucumber/CucumberSetBrowserViaTagFail.java
@@ -7,7 +7,8 @@ import com.xceptance.neodymium.NeodymiumCucumberRunner;
 import cucumber.api.CucumberOptions;
 
 @RunWith(NeodymiumCucumberRunner.class)
-@CucumberOptions(features = "src/test/resources/com/xceptance/neodymium/testclasses/cucumber/CucumberSetBrowserViaTagFail.feature", glue = "com/xceptance/neodymium/testclasses/cucumber")
+@CucumberOptions(features = "src/test/resources/com/xceptance/neodymium/testclasses/cucumber/CucumberSetBrowserViaTagFail.feature",
+                 glue = "com/xceptance/neodymium/testclasses/cucumber", plugin = "null_summary")
 public class CucumberSetBrowserViaTagFail
 {
 }
