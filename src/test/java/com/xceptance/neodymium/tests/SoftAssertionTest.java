dc9e99633562a5e1a3185955cbc4b23109b73411
Marcel Pfotenhauer <m.pfotenhauer@xceptance.net>
2018-10-12 09:04:34 +0200

    refactured test structure for soft assertions

--- src/test/java/com/xceptance/neodymium/tests/SoftAssertionTest.java
+++ src/test/java/com/xceptance/neodymium/tests/SoftAssertionTest.java
@@ -1,43 +1,17 @@
 package com.xceptance.neodymium.tests;
 
-import static com.codeborne.selenide.Condition.exist;
-import static com.codeborne.selenide.Selenide.$;
-
-import org.junit.Assert;
-import org.junit.Rule;
 import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
 
-import com.codeborne.selenide.Configuration;
-import com.codeborne.selenide.Configuration.AssertionMode;
-import com.codeborne.selenide.Selenide;
-import com.codeborne.selenide.ex.ElementNotFound;
-import com.codeborne.selenide.junit.SoftAsserts;
-import com.xceptance.neodymium.NeodymiumRunner;
-import com.xceptance.neodymium.module.statement.browser.multibrowser.Browser;
-import com.xceptance.neodymium.util.Neodymium;
+import com.xceptance.neodymium.testclasses.softassertion.UseSoftAssertions;
 
-@RunWith(NeodymiumRunner.class)
-@Browser("Chrome_headless")
-public class SoftAssertionTest
+public class SoftAssertionTest extends NeodymiumTest
 {
-    @Rule
-    public SoftAsserts softAsserts = new SoftAsserts();
-
-    @Test(expected = ElementNotFound.class)
+    @Test
     public void validateSoftAssertion()
     {
-        Neodymium.softAssertions(true);
-        Selenide.open("https://blog.xceptance.com/");
-
-        Assert.assertEquals(Configuration.assertionMode, AssertionMode.SOFT);
-        $("#notFound1").should(exist);
-        $("#notFound2").should(exist);
-        $("#masthead .search-toggle").click();
-        $("#notFound3").should(exist);
-        $("#notFound4").click();
-
-        // This should not be called since
-        throw new NullPointerException();
+        Result result = JUnitCore.runClasses(UseSoftAssertions.class);
+        checkFail(result, 1, 0, 1, null);
     }
 }
