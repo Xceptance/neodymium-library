414a50475bf9a4f0d89168ba8983c15b4fb3970e
Olha Omelianchuk <o.omelianchuk@xceptance.net>
2021-01-22 12:00:00 +0100

    #153 increase number of data sets to reduce probability of test failure

--- src/test/java/com/xceptance/neodymium/testclasses/data/RandomnessOfDataSets.java
+++ src/test/java/com/xceptance/neodymium/testclasses/data/RandomnessOfDataSets.java
@@ -12,13 +12,16 @@ import com.xceptance.neodymium.module.statement.testdata.DataSet;
 import com.xceptance.neodymium.module.statement.testdata.SuppressDataSets;
 import com.xceptance.neodymium.util.DataUtils;
 
+import io.qameta.allure.junit4.Tag;
+
+@Tag("This test may fail but the probability for it is very low")
 @RunWith(NeodymiumRunner.class)
 public class RandomnessOfDataSets
 {
     private static List<String> datasets = new ArrayList<String>();
 
     @Test
-    @DataSet(randomSets = 4)
+    @DataSet(randomSets = 10)
     public void test1()
     {
         // assert test data is available for the test
@@ -30,6 +33,7 @@ public class RandomnessOfDataSets
     @SuppressDataSets
     public void test2()
     {
+        // assert that at least one of data sets is not on the same position as it stays in the test data sheet
         boolean changedOrder = false;
         for (int i = 1; i < datasets.size(); i++)
         {
