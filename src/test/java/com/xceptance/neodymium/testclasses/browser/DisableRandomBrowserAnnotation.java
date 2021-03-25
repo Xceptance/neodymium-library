2873be0ba8d084a0b2e62434768a4894fe13861d
Olha Omelianchuk <o.omelianchuk@xceptance.net>
2021-03-25 12:40:51 +0100

    add browsers to disable

--- src/test/java/com/xceptance/neodymium/testclasses/browser/DisableRandomBrowserAnnotation.java
+++ src/test/java/com/xceptance/neodymium/testclasses/browser/DisableRandomBrowserAnnotation.java
@@ -4,9 +4,12 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import com.xceptance.neodymium.NeodymiumRunner;
+import com.xceptance.neodymium.module.statement.browser.multibrowser.Browser;
 import com.xceptance.neodymium.module.statement.browser.multibrowser.RandomBrowsers;
 
 @RunWith(NeodymiumRunner.class)
+@Browser("Chrome_1024x768")
+@Browser("Chrome_1500x1000")
 public class DisableRandomBrowserAnnotation
 {
     @RandomBrowsers(0)
