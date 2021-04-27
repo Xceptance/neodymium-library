bfd0fc552708ac7bd6ee6709d45f6076d7362067
Olha Omelianchuk <o.omelianchuk@xceptance.net>
2021-04-27 16:30:52 +0200

    rename browserstack password to browserstack api key

--- src/test/java/com/xceptance/neodymium/util/TestConfiguration.java
+++ src/test/java/com/xceptance/neodymium/util/TestConfiguration.java
@@ -17,6 +17,6 @@ public interface TestConfiguration extends Mutable
     @Key("BROWSERSTACK_USERNAME")
     public String browserstackUsername();
 
-    @Key("BROWSERSTACK_PASSWORD")
-    public String browserstackPassword();
+    @Key("BROWSERSTACK_API_KEY")
+    public String browserstackApiKey();
 }
