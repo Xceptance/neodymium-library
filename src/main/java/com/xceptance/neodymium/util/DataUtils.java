7ca3e41da197a78757f662be9bed3018fbb536f4
Olha Omelianchuk <o.omelianchuk@xceptance.net>
2021-05-05 13:12:50 +0200

    remove unneeded import

--- src/main/java/com/xceptance/neodymium/util/DataUtils.java
+++ src/main/java/com/xceptance/neodymium/util/DataUtils.java
@@ -10,7 +10,6 @@ import org.apache.commons.text.TextRandomProvider;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import com.google.gson.JsonPrimitive;
