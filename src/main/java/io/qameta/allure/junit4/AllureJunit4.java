cb3e61e77027aefe4168bd588972ef67c96f9f5c
Olha Omelianchuk <o.omelianchuk@xceptance.net>
2021-05-05 16:22:27 +0200

    patch the solution #164

---
+++ src/main/java/io/qameta/allure/junit4/AllureJunit4.java
@@ -0,0 +1,230 @@
+/*
+ *  Copyright 2019 Qameta Software OÜ
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package io.qameta.allure.junit4;
+
+import io.qameta.allure.Allure;
+import io.qameta.allure.AllureLifecycle;
+import io.qameta.allure.model.Label;
+import io.qameta.allure.model.Link;
+import io.qameta.allure.model.Status;
+import io.qameta.allure.model.StatusDetails;
+import io.qameta.allure.model.TestResult;
+import io.qameta.allure.util.AnnotationUtils;
+
+import org.junit.Ignore;
+import org.junit.runner.Description;
+import org.junit.runner.Result;
+import org.junit.runner.notification.Failure;
+import org.junit.runner.notification.RunListener;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.UUID;
+
+import static io.qameta.allure.util.AnnotationUtils.getLabels;
+import static io.qameta.allure.util.AnnotationUtils.getLinks;
+import static io.qameta.allure.util.ResultsUtils.createFrameworkLabel;
+import static io.qameta.allure.util.ResultsUtils.createHostLabel;
+import static io.qameta.allure.util.ResultsUtils.createLanguageLabel;
+import static io.qameta.allure.util.ResultsUtils.createPackageLabel;
+import static io.qameta.allure.util.ResultsUtils.createSuiteLabel;
+import static io.qameta.allure.util.ResultsUtils.createTestClassLabel;
+import static io.qameta.allure.util.ResultsUtils.createTestMethodLabel;
+import static io.qameta.allure.util.ResultsUtils.createThreadLabel;
+import static io.qameta.allure.util.ResultsUtils.getProvidedLabels;
+import static io.qameta.allure.util.ResultsUtils.getStatus;
+import static io.qameta.allure.util.ResultsUtils.getStatusDetails;
+import static io.qameta.allure.util.ResultsUtils.md5;
+
+/**
+ * Allure Junit4 listener.
+ */
+@RunListener.ThreadSafe
+@SuppressWarnings({"PMD.ExcessiveImports", "PMD.CouplingBetweenObjects", "checkstyle:ClassFanOutComplexity"})
+public class AllureJunit4 extends RunListener {
+
+    private final ThreadLocal<String> testCases = new InheritableThreadLocal<String>() {
+        @Override
+        protected String initialValue() {
+            return UUID.randomUUID().toString();
+        }
+    };
+    
+    private final AllureLifecycle lifecycle;
+
+    public AllureJunit4() {
+        this(Allure.getLifecycle());
+    }
+
+    public AllureJunit4(final AllureLifecycle lifecycle) {
+        this.lifecycle = lifecycle;
+    }
+
+    public AllureLifecycle getLifecycle() {
+        return lifecycle;
+    }
+
+    @Override
+    public void testRunStarted(final Description description) {
+        //do nothing
+    }
+
+    @Override
+    public void testRunFinished(final Result result) {
+        //do nothing
+    }
+
+    @Override
+    public void testStarted(final Description description) {
+        final String uuid = testCases.get();
+        final TestResult result = createTestResult(uuid, description);
+        getLifecycle().scheduleTestCase(result);
+        getLifecycle().startTestCase(uuid);
+    }
+
+    @Override
+    public void testFinished(final Description description) {
+        final String uuid = testCases.get();
+        testCases.remove();
+        getLifecycle().updateTestCase(uuid, testResult -> {
+            if (Objects.isNull(testResult.getStatus())) {
+                testResult.setStatus(Status.PASSED);
+            }
+        });
+
+        getLifecycle().stopTestCase(uuid);
+        getLifecycle().writeTestCase(uuid);
+    }
+
+    @Override
+    public void testFailure(final Failure failure) {
+        final String uuid = testCases.get();
+        getLifecycle().updateTestCase(uuid, testResult -> {
+            if (Objects.isNull(testResult.getStatus())) {
+                testResult.setStatus(getStatus(failure.getException()).orElse(null))
+               .setStatusDetails(getStatusDetails(failure.getException()).orElse(null));
+            }
+        });
+    }
+
+    @Override
+    public void testAssumptionFailure(final Failure failure) {
+        final String uuid = testCases.get();
+        getLifecycle().updateTestCase(uuid, testResult ->
+                testResult.setStatus(Status.SKIPPED)
+                        .setStatusDetails(getStatusDetails(failure.getException()).orElse(null))
+        );
+    }
+
+    @Override
+    public void testIgnored(final Description description) {
+        final String uuid = testCases.get();
+        testCases.remove();
+
+        final TestResult result = createTestResult(uuid, description);
+        result.setStatus(Status.SKIPPED);
+        result.setStatusDetails(getIgnoredMessage(description));
+        result.setStart(System.currentTimeMillis());
+
+        getLifecycle().scheduleTestCase(result);
+        getLifecycle().stopTestCase(uuid);
+        getLifecycle().writeTestCase(uuid);
+    }
+
+    private Optional<String> getDisplayName(final Description result) {
+        return Optional.ofNullable(result.getAnnotation(DisplayName.class))
+                .map(DisplayName::value);
+    }
+
+    private Optional<String> getDescription(final Description result) {
+        return Optional.ofNullable(result.getAnnotation(io.qameta.allure.Description.class))
+                .map(io.qameta.allure.Description::value);
+    }
+
+    private List<Link> extractLinks(final Description description) {
+        final List<Link> result = new ArrayList<>(getLinks(description.getAnnotations()));
+        Optional.of(description)
+                .map(Description::getTestClass)
+                .map(AnnotationUtils::getLinks)
+                .ifPresent(result::addAll);
+        return result;
+    }
+
+    private List<Label> extractLabels(final Description description) {
+        final List<Label> result = new ArrayList<>(getLabels(description.getAnnotations()));
+        Optional.of(description)
+                .map(Description::getTestClass)
+                .map(AnnotationUtils::getLabels)
+                .ifPresent(result::addAll);
+        return result;
+    }
+
+    private String getHistoryId(final Description description) {
+        return md5(description.getClassName() + description.getMethodName());
+    }
+
+    private String getPackage(final Class<?> testClass) {
+        return Optional.ofNullable(testClass)
+                .map(Class::getPackage)
+                .map(Package::getName)
+                .orElse("");
+    }
+
+    private StatusDetails getIgnoredMessage(final Description description) {
+        final Ignore ignore = description.getAnnotation(Ignore.class);
+        final String message = Objects.nonNull(ignore) && !ignore.value().isEmpty()
+                ? ignore.value() : "Test ignored (without reason)!";
+        return new StatusDetails().setMessage(message);
+    }
+
+    private TestResult createTestResult(final String uuid, final Description description) {
+        final String className = description.getClassName();
+        final String methodName = description.getMethodName();
+        final String name = Objects.nonNull(methodName) ? methodName : className;
+        final String fullName = Objects.nonNull(methodName) ? String.format("%s.%s", className, methodName) : className;
+        final String suite = Optional.ofNullable(description.getTestClass())
+                .map(it -> it.getAnnotation(DisplayName.class))
+                .map(DisplayName::value).orElse(className);
+
+        final TestResult testResult = new TestResult()
+                .setUuid(uuid)
+                .setHistoryId(getHistoryId(description))
+                .setFullName(fullName)
+                .setName(name);
+
+        testResult.getLabels().addAll(getProvidedLabels());
+        testResult.getLabels().addAll(Arrays.asList(
+                createPackageLabel(getPackage(description.getTestClass())),
+                createTestClassLabel(className),
+                createTestMethodLabel(name),
+                createSuiteLabel(suite),
+                createHostLabel(),
+                createThreadLabel(),
+                createFrameworkLabel("junit4"),
+                createLanguageLabel("java")
+        ));
+        testResult.getLabels().addAll(extractLabels(description));
+        testResult.getLinks().addAll(extractLinks(description));
+
+        getDisplayName(description).ifPresent(testResult::setName);
+        getDescription(description).ifPresent(testResult::setDescription);
+        return testResult;
+    }
+
+}
---
+++ src/main/java/io/qameta/allure/selenide/AllureSelenide.java
@@ -0,0 +1,198 @@
+/*
+ *  Copyright 2019 Qameta Software OÜ
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package io.qameta.allure.selenide;
+
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.WebDriverRunner;
+import com.codeborne.selenide.logevents.LogEvent;
+import com.codeborne.selenide.logevents.LogEventListener;
+import com.codeborne.selenide.logevents.SelenideLog;
+import io.qameta.allure.Allure;
+import io.qameta.allure.AllureLifecycle;
+import io.qameta.allure.model.Status;
+import io.qameta.allure.model.StatusDetails;
+import io.qameta.allure.model.StepResult;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriverException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+import java.util.UUID;
+import java.util.logging.Level;
+
+import static io.qameta.allure.util.ResultsUtils.getStatus;
+import static io.qameta.allure.util.ResultsUtils.getStatusDetails;
+import static java.nio.charset.StandardCharsets.UTF_8;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
+/**
+ * @author Artem Eroshenko.
+ */
+@SuppressWarnings("unused")
+public class AllureSelenide implements LogEventListener {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(AllureSelenide.class);
+
+    private boolean saveScreenshots = true;
+    private boolean savePageHtml = true;
+    private boolean saveStacktrace = true;
+    private boolean includeSelenideLocatorsSteps = true;
+    private final Map<LogType, Level> logTypesToSave = new HashMap<>();
+    private final AllureLifecycle lifecycle;
+
+    public AllureSelenide() {
+        this(Allure.getLifecycle());
+    }
+
+    public AllureSelenide(final AllureLifecycle lifecycle) {
+        this.lifecycle = lifecycle;
+    }
+
+    public AllureSelenide screenshots(final boolean saveScreenshots) {
+        this.saveScreenshots = saveScreenshots;
+        return this;
+    }
+
+    public AllureSelenide savePageSource(final boolean savePageHtml) {
+        this.savePageHtml = savePageHtml;
+        return this;
+    }
+    
+    public AllureSelenide saveFailureStacktrace(final boolean saveStacktrace) {
+        this.saveStacktrace = saveStacktrace;
+        return this;
+    }
+
+    public AllureSelenide includeSelenideSteps(final boolean includeSelenideSteps) {
+        this.includeSelenideLocatorsSteps = includeSelenideSteps;
+        return this;
+    }
+
+    public AllureSelenide enableLogs(final LogType logType, final Level logLevel) {
+        logTypesToSave.put(logType, logLevel);
+
+        return this;
+    }
+
+    public AllureSelenide disableLogs(final LogType logType) {
+        logTypesToSave.remove(logType);
+
+        return this;
+    }
+
+    private static Optional<byte[]> getScreenshotBytes() {
+        try {
+            return WebDriverRunner.hasWebDriverStarted()
+                ? Optional.of(((TakesScreenshot) WebDriverRunner.getWebDriver()).getScreenshotAs(OutputType.BYTES))
+                : Optional.empty();
+        } catch (WebDriverException e) {
+            LOGGER.warn("Could not get screen shot", e);
+            return Optional.empty();
+        }
+    }
+
+    private static Optional<byte[]> getPageSourceBytes() {
+        try {
+            return WebDriverRunner.hasWebDriverStarted()
+                ? Optional.of(WebDriverRunner.getWebDriver().getPageSource().getBytes(UTF_8))
+                : Optional.empty();
+        } catch (WebDriverException e) {
+            LOGGER.warn("Could not get page source", e);
+            return Optional.empty();
+        }
+    }
+
+    private static String getBrowserLogs(final LogType logType, final Level level) {
+        return String.join("\n\n", Selenide.getWebDriverLogs(logType.toString(), level));
+    }
+
+    @Override
+    public void beforeEvent(final LogEvent event) {
+        if (stepsShouldBeLogged(event)) {
+            lifecycle.getCurrentTestCaseOrStep().ifPresent(parentUuid -> {
+                final String uuid = UUID.randomUUID().toString();
+                lifecycle.startStep(parentUuid, uuid, new StepResult().setName(event.toString()));
+            });
+        }
+    }
+
+    @Override
+    public void afterEvent(final LogEvent event) {
+        if (event.getStatus().equals(LogEvent.EventStatus.FAIL)) {
+            lifecycle.getCurrentTestCaseOrStep().ifPresent(parentUuid -> {
+                if (saveScreenshots) {
+                    getScreenshotBytes()
+                            .ifPresent(bytes -> lifecycle.addAttachment("Screenshot", "image/png", "png", bytes));
+                }
+                if (savePageHtml) {
+                    getPageSourceBytes()
+                            .ifPresent(bytes -> lifecycle.addAttachment("Page source", "text/html", "html", bytes));
+                }
+                if (saveStacktrace) {
+                   final String failureDetails = new StringBuffer()
+                        .append(Optional.ofNullable(event.getError().getMessage())
+                        .orElse(event.getError().getClass().getName())).append('\n')
+                        .append(getStackTraceAsString(event.getError())).toString();
+                    
+                   Allure.addAttachment("Information about step failure: ", failureDetails);
+                }
+                if (!logTypesToSave.isEmpty()) {
+                    logTypesToSave
+                            .forEach((logType, level) -> {
+                                final byte[] content = getBrowserLogs(logType, level).getBytes(UTF_8);
+                                lifecycle.addAttachment("Logs from: " + logType, "application/json", ".txt", content);
+                            });
+                }
+            });
+        }
+
+        if (stepsShouldBeLogged(event)) {
+            lifecycle.getCurrentTestCaseOrStep().ifPresent(parentUuid -> {
+                switch (event.getStatus()) {
+                    case PASS:
+                        lifecycle.updateStep(step -> step.setStatus(Status.PASSED));
+                        break;
+                    case FAIL:
+                        lifecycle.updateStep(stepResult -> {
+                            stepResult.setStatus(getStatus(event.getError()).orElse(Status.BROKEN));
+                            stepResult.setStatusDetails(getStatusDetails(event.getError()).orElse(new StatusDetails()));
+                        });
+                        break;
+                    default:
+                        LOGGER.warn("Step finished with unsupported status {}", event.getStatus());
+                        break;
+                }
+                lifecycle.stopStep();
+            });
+        }
+    }
+
+    private String getStackTraceAsString(final Throwable throwable) {
+        final StringWriter stringWriter = new StringWriter();
+        throwable.printStackTrace(new PrintWriter(stringWriter));
+        return stringWriter.toString();
+    }
+    private boolean stepsShouldBeLogged(final LogEvent event) {
+        //  other customer Loggers could be configured, they should be logged
+        return includeSelenideLocatorsSteps || !(event instanceof SelenideLog);
+    }
+}
