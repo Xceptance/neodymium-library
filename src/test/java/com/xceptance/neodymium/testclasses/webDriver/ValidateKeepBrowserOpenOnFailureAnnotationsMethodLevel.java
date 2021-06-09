d41a3bad1aed79dc3fee33097f1bac83b73832aa
gkunze <gkunze@xceptance.net>
2021-06-09 15:06:00 +0200

    #103 Added Testcases

---
+++ src/test/java/com/xceptance/neodymium/testclasses/webDriver/ValidateKeepBrowserOpenOnFailureAnnotationsMethodLevel.java
@@ -0,0 +1,104 @@
+package com.xceptance.neodymium.testclasses.webDriver;
+
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.openqa.selenium.WebDriver;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Selenide;
+import com.xceptance.neodymium.NeodymiumRunner;
+import com.xceptance.neodymium.module.statement.browser.KeepBrowserOpen;
+import com.xceptance.neodymium.module.statement.browser.multibrowser.Browser;
+import com.xceptance.neodymium.module.statement.browser.multibrowser.WebDriverCache;
+import com.xceptance.neodymium.tests.NeodymiumWebDriverTest;
+import com.xceptance.neodymium.util.Neodymium;
+
+@RunWith(NeodymiumRunner.class)
+@Browser("Chrome_1024x768")
+public class ValidateKeepBrowserOpenOnFailureAnnotationsMethodLevel
+{
+    private static WebDriver webDriver1;
+
+    private static WebDriver webDriver2;
+
+    private static WebDriver webDriver3;
+
+    @BeforeClass
+    public static void beforeClass()
+    {
+        Assert.assertNull(webDriver1);
+        Assert.assertNull(Neodymium.getDriver());
+    }
+
+    @Before
+    public void before()
+    {
+        if (webDriver1 == null)
+        {
+            webDriver1 = Neodymium.getDriver();
+        }
+        else if (webDriver2 == null)
+        {
+            webDriver2 = Neodymium.getDriver();
+        }
+        else if (webDriver3 == null)
+        {
+            webDriver3 = Neodymium.getDriver();
+        }
+        else
+        {
+            Assert.assertNotNull(Neodymium.getDriver());
+        }
+        Assert.assertNotNull(webDriver1);
+    }
+
+    @Test
+    @KeepBrowserOpen(true)
+    public void test1()
+    {
+        Assert.assertEquals(webDriver1, Neodymium.getDriver());
+        NeodymiumWebDriverTest.assertWebDriverAlive(webDriver1);
+    }
+
+    @Test
+    @KeepBrowserOpen(true)
+    public void test2()
+    {
+        Assert.assertNotEquals(webDriver1, webDriver2);
+        Assert.assertEquals(webDriver2, Neodymium.getDriver());
+        NeodymiumWebDriverTest.assertWebDriverClosed(webDriver1);
+        NeodymiumWebDriverTest.assertWebDriverAlive(webDriver2);
+
+        // Let condition fail so that the WebDriver/browser is kept open
+        Selenide.$("#cantFindMe").should(Condition.exist);
+    }
+
+    @Test
+    @KeepBrowserOpen(true)
+    public void test3()
+    {
+        Assert.assertNotEquals(webDriver1, webDriver2);
+        Assert.assertNotEquals(webDriver2, webDriver3);
+        Assert.assertNotEquals(webDriver1, webDriver3);
+        Assert.assertEquals(webDriver3, Neodymium.getDriver());
+        NeodymiumWebDriverTest.assertWebDriverClosed(webDriver1);
+        NeodymiumWebDriverTest.assertWebDriverAlive(webDriver2);
+        NeodymiumWebDriverTest.assertWebDriverAlive(webDriver3);
+    }
+
+    @AfterClass
+    public static void afterClass()
+    {
+        Assert.assertEquals(0, WebDriverCache.instance.getWebDriverStateContainerCacheSize());
+
+        NeodymiumWebDriverTest.assertWebDriverAlive(webDriver2);
+        webDriver2.quit();
+        NeodymiumWebDriverTest.assertWebDriverClosed(webDriver1);
+        NeodymiumWebDriverTest.assertWebDriverClosed(webDriver2);
+        NeodymiumWebDriverTest.assertWebDriverClosed(webDriver3);
+    }
+}
