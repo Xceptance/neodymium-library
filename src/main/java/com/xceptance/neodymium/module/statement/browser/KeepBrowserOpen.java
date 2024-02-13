d611d23c97eeed3170d02f7b6f9220656e36907f
Jonas Hoyer <j.hoyer@xceptance.com>
2024-02-13 15:00:30 +0100

    javadoc update KeepBrowserOpen annotation

--- src/main/java/com/xceptance/neodymium/module/statement/browser/KeepBrowserOpen.java
+++ src/main/java/com/xceptance/neodymium/module/statement/browser/KeepBrowserOpen.java
@@ -13,10 +13,10 @@ import java.lang.annotation.Target;
   TYPE, METHOD
 })
 /**
- * This annotation defines whether or not the browser should be closed if the annotated test fails and is set to "false" per default.
+ * This annotation defines whether or not the browser should stay open if the annotated test fails and is set to "false" per default.
  * It can be used for test methods and test classes. 
  * If it's used for both, then the method level annotation overrides the class level annotation.
- * Also, if used, the method level annotation and the class level annotation both override the corresponding configuration property.
+ * Also, if used, the method level annotation and the class level annotation both override the corresponding neodymium configuration property (neodymium.webDriver.keepBrowserOpenOnFailure).
  */
 public @interface KeepBrowserOpen
 {
