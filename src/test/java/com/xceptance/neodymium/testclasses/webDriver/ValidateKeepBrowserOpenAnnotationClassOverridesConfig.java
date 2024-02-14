ed6ae0d490b992bbb3264b2f744bd112911d0c88
Jonas Hoyer <j.hoyer@xceptance.com>
2024-02-14 15:36:43 +0100

    added ValidateKeepBrowserOpenAnnotationMethodOverridesConfig and ValidateKeepBrowserOpenAnnotationClassOverridesConfig

--- src/test/java/com/xceptance/neodymium/testclasses/webDriver/ValidateKeepBrowserOpenAnnotationClassOverridesConfig.java
+++ src/test/java/com/xceptance/neodymium/testclasses/webDriver/ValidateKeepBrowserOpenAnnotationClassOverridesConfig.java
@@ -1,5 +1,10 @@
 package com.xceptance.neodymium.testclasses.webDriver;
 
+import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.aeonbits.owner.ConfigFactory;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Assert;
@@ -11,24 +16,41 @@ import org.junit.runner.RunWith;
 import org.junit.runners.MethodSorters;
 import org.openqa.selenium.WebDriver;
 
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Selenide;
 import com.xceptance.neodymium.NeodymiumRunner;
 import com.xceptance.neodymium.module.statement.browser.KeepBrowserOpen;
 import com.xceptance.neodymium.module.statement.browser.multibrowser.Browser;
 import com.xceptance.neodymium.module.statement.browser.multibrowser.WebDriverCache;
+import com.xceptance.neodymium.tests.NeodymiumTest;
 import com.xceptance.neodymium.tests.NeodymiumWebDriverTest;
 import com.xceptance.neodymium.util.Neodymium;
 
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 @RunWith(NeodymiumRunner.class)
-public class ValidateKeepBrowserOpenAnnotationsMethodLevel
+@KeepBrowserOpen(onlyOnFailure = false)
+@Browser("Chrome_1024x768")
+public class ValidateKeepBrowserOpenAnnotationClassOverridesConfig
 {
     private static WebDriver webDriver1;
 
     private static WebDriver webDriver2;
 
+    private static WebDriver webDriver3;
+
+    private static File tempConfigFile;
+
     @BeforeClass
     public static void beforeClass()
     {
+        // set up a temporary neodymium.properties
+        final String fileLocation = "config/temp-ValidateKeepWebDriverOpen-neodymium.properties";
+        tempConfigFile = new File("./" + fileLocation);
+        Map<String, String> properties = new HashMap<>();
+        properties.put("neodymium.webDriver.keepBrowserOpenOnFailure", "true");
+        NeodymiumTest.writeMapToPropertiesFile(properties, tempConfigFile);
+        ConfigFactory.setProperty(Neodymium.TEMPORARY_CONFIG_FILE_PROPERTY_NAME, "file:" + fileLocation);
+
         Assert.assertNull(webDriver1);
         Assert.assertNull(Neodymium.getDriver());
     }
@@ -44,6 +66,10 @@ public class ValidateKeepBrowserOpenAnnotationsMethodLevel
         {
             webDriver2 = Neodymium.getDriver();
         }
+        else if (webDriver3 == null)
+        {
+            webDriver3 = Neodymium.getDriver();
+        }
         else
         {
             Assert.assertNotNull(Neodymium.getDriver());
@@ -52,8 +78,6 @@ public class ValidateKeepBrowserOpenAnnotationsMethodLevel
     }
 
     @Test
-    @Browser("Chrome_1024x768")
-    @KeepBrowserOpen(onlyOnFailure = false)
     public void test1()
     {
         Assert.assertEquals(webDriver1, Neodymium.getDriver());
@@ -61,14 +85,27 @@ public class ValidateKeepBrowserOpenAnnotationsMethodLevel
     }
 
     @Test
-    @Browser("Chrome_1024x768")
-    @KeepBrowserOpen(onlyOnFailure = false)
     public void test2()
     {
         Assert.assertNotEquals(webDriver1, webDriver2);
         Assert.assertEquals(webDriver2, Neodymium.getDriver());
         NeodymiumWebDriverTest.assertWebDriverAlive(webDriver1);
         NeodymiumWebDriverTest.assertWebDriverAlive(webDriver2);
+        
+        // Let condition fail so that the WebDriver/browser is kept open
+        Selenide.$("#cantFindMe").should(Condition.exist);
+    }
+    
+    @Test
+    public void test3()
+    {
+        Assert.assertNotEquals(webDriver1, webDriver2);
+        Assert.assertNotEquals(webDriver2, webDriver3);
+        Assert.assertNotEquals(webDriver1, webDriver3);
+        Assert.assertEquals(webDriver3, Neodymium.getDriver());
+        NeodymiumWebDriverTest.assertWebDriverAlive(webDriver1);
+        NeodymiumWebDriverTest.assertWebDriverAlive(webDriver2);
+        NeodymiumWebDriverTest.assertWebDriverAlive(webDriver3);
     }
 
     @After
@@ -84,9 +121,12 @@ public class ValidateKeepBrowserOpenAnnotationsMethodLevel
 
         NeodymiumWebDriverTest.assertWebDriverAlive(webDriver1);
         NeodymiumWebDriverTest.assertWebDriverAlive(webDriver2);
+        NeodymiumWebDriverTest.assertWebDriverAlive(webDriver3);
         webDriver1.quit();
         webDriver2.quit();
+        webDriver3.quit();
         NeodymiumWebDriverTest.assertWebDriverClosed(webDriver1);
         NeodymiumWebDriverTest.assertWebDriverClosed(webDriver2);
+        NeodymiumWebDriverTest.assertWebDriverClosed(webDriver3);
     }
 }
