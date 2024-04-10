4169d509b46049a080c8305fc17ba9e74aefd015
Bernd Weigel <b.weigel@xceptance.com>
2024-04-10 16:04:59 +0200

    HotFix: JavaDoc change to handle javaDoc Generation issue.

--- src/main/java/com/xceptance/neodymium/WorkInProgress.java
+++ src/main/java/com/xceptance/neodymium/WorkInProgress.java
@@ -2,14 +2,15 @@ package com.xceptance.neodymium;
 
 import static java.lang.annotation.ElementType.METHOD;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
 /**
- * Works like the opposite of @Ignore. If tests in a class are annotated with @WorkInProgress,
- * only those tests will be executed. All other tests of the class, not being annotated with 
- * @WorkInProgress, get skipped. The @WorkInProgress annotation is per default disabled but can
- * be enabled via neodymium property "workInProgress".
+ * Works like the opposite of {@code @Ignore}. If tests in a class are annotated with {@code @WorkInProgress}, only
+ * those tests will be executed. All other tests of the class, not being annotated with {@code @WorkInProgress}, get
+ * skipped. The {@code @WorkInProgress} annotation is per default disabled but can be enabled via Neodymium property
+ * "workInProgress".
  */
 @Retention(RUNTIME)
 @Target(
