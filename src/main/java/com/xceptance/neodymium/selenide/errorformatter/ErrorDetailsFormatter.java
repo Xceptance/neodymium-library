ab9cdcb1cd48fcb510692d9e0e657af5030dda34
Bernd Weigel <b.weigel@xceptance.com>
2024-08-02 09:29:44 +0200

    [#280] added error formatter and according configs

---
+++ src/main/java/com/xceptance/neodymium/selenide/errorformatter/ErrorDetailsFormatter.java
@@ -0,0 +1,32 @@
+package com.xceptance.neodymium.selenide.errorformatter;
+
+import javax.annotation.Nonnull;
+
+import com.codeborne.selenide.Driver;
+import com.codeborne.selenide.ex.SelenideErrorFormatter;
+import com.codeborne.selenide.ex.Strings;
+import com.codeborne.selenide.impl.Screenshot;
+import com.xceptance.neodymium.util.Neodymium;
+
+public class ErrorDetailsFormatter extends SelenideErrorFormatter
+{
+    @Nonnull
+    @Override
+    public String generateErrorDetails(AssertionError error, Driver driver, Screenshot screenshot, long timeoutMs)
+    {
+
+        if (Neodymium.configuration().showSelenideErrorDetails())
+        {
+            return Strings.join(super.generateErrorDetails(error, driver, screenshot, timeoutMs));
+        }
+
+        // The screenshot path is always unique. Also the caused by message is dependent on the browser, so just ignore
+        // those.
+        // Timeout might be interesting, but to keep the message clean we add it as a step parameter to the current step
+        StringBuilder sb = new StringBuilder();
+        return sb.append("(")
+                 .append(timeout(timeoutMs))
+                 .append(")")
+                 .toString();
+    }
+}
