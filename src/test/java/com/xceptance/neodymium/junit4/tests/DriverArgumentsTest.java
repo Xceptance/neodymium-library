674212cd6d5a1b2d81edcfd3ec16a771181701d1
Olha Omelianchuk <o.omelianchuk@xceptance.net>
2024-09-11 15:06:44 +0200

    [#198] add a handling to not get the dev-neodymium.properties removed

--- src/test/java/com/xceptance/neodymium/junit4/tests/DriverArgumentsTest.java
+++ src/test/java/com/xceptance/neodymium/junit4/tests/DriverArgumentsTest.java
@@ -1,6 +1,7 @@
 package com.xceptance.neodymium.junit4.tests;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
@@ -10,6 +11,7 @@ import org.junit.Test;
 import org.junit.runner.JUnitCore;
 import org.junit.runner.Result;
 
+import com.xceptance.neodymium.common.browser.configuration.MultibrowserConfiguration;
 import com.xceptance.neodymium.junit4.testclasses.webDriver.DriverArgumentsConfigTest;
 
 public class DriverArgumentsTest extends NeodymiumTest
@@ -25,29 +27,33 @@ public class DriverArgumentsTest extends NeodymiumTest
     public static String firefoxWebsocketPort = "8785";
 
     @BeforeClass
-    public static void createSettings()
+    public static void createSettings() throws IOException
     {
-        Map<String, String> properties3 = new HashMap<>();
+        Map<String, String> properties = new HashMap<>();
 
-        properties3.put("browserprofile.FF_with_args.name", "FF with args");
-        properties3.put("browserprofile.FF_with_args.headless", "true");
-        properties3.put("browserprofile.FF_with_args.browserResolution", "1024x768");
-        properties3.put("browserprofile.FF_with_args.browser", "firefox");
-        properties3.put("browserprofile.FF_with_args.driverArgs",
-                        "--log ; info ;--allow-hosts; xceptance.com; google.com; --port=" + firefoxPort
-                                                                  + "; --log-no-truncate ; --websocket-port=" + firefoxWebsocketPort
-                                                                  + "; --allow-origins; http://xceptance.com:8785 ; --log-path="
-                                                                  + logFileNameFirefox);
+        properties.put("browserprofile.FF_with_args.name", "FF with args");
+        properties.put("browserprofile.FF_with_args.headless", "true");
+        properties.put("browserprofile.FF_with_args.browserResolution", "1024x768");
+        properties.put("browserprofile.FF_with_args.browser", "firefox");
+        properties.put("browserprofile.FF_with_args.driverArgs",
+                       "--log ; info ;--allow-hosts; xceptance.com; google.com; --port=" + firefoxPort
+                                                                 + "; --log-no-truncate ; --websocket-port=" + firefoxWebsocketPort
+                                                                 + "; --allow-origins; http://xceptance.com:8785 ; --log-path="
+                                                                 + logFileNameFirefox);
 
-        properties3.put("browserprofile.Chrome_with_args.name", "Chrome with args");
-        properties3.put("browserprofile.Chrome_with_args.headless", "true");
-        properties3.put("browserprofile.Chrome_with_args.browserResolution", "1024x768");
-        properties3.put("browserprofile.Chrome_with_args.browser", "chrome");
-        properties3.put("browserprofile.Chrome_with_args.driverArgs",
-                        "--port=" + chromePort + "; --allowed-origins=localhost, xceptance.com; --log-level=INFO ; --log-path=" + logFileNameChrome);
-        File tempConfigFile3 = new File("./config/dev-browser.properties");
-        writeMapToPropertiesFile(properties3, tempConfigFile3);
-        tempFiles.add(tempConfigFile3);
+        properties.put("browserprofile.Chrome_with_args.name", "Chrome with args");
+        properties.put("browserprofile.Chrome_with_args.headless", "true");
+        properties.put("browserprofile.Chrome_with_args.browserResolution", "1024x768");
+        properties.put("browserprofile.Chrome_with_args.browser", "chrome");
+        properties.put("browserprofile.Chrome_with_args.driverArgs",
+                       "--port=" + chromePort + "; --allowed-origins=localhost, xceptance.com; --log-level=INFO ; --log-path=" + logFileNameChrome);
+        File tempConfigFile = File.createTempFile("driverArgumentsTest", "", new File("./config/"));
+        writeMapToPropertiesFile(properties, tempConfigFile);
+        tempFiles.add(tempConfigFile);
+
+        // this line is important as we initialize the config from the temporary file we created above
+        MultibrowserConfiguration.clearAllInstances();
+        MultibrowserConfiguration.getInstance(tempConfigFile.getPath());
     }
 
     @Test
