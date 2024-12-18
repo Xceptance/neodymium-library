682a04060684f140e329b3fb0f58e50e720c3e22
Olha Omelianchuk <o.omelianchuk@xceptance.net>
2024-12-18 11:09:08 +0100

    [#330] fix AllureSelenideListenerTest

--- src/test/java/com/xceptance/neodymium/junit4/testclasses/cucumber/CucumberValidateAllureSelenideListenerIsActive.java
+++ src/test/java/com/xceptance/neodymium/junit4/testclasses/cucumber/CucumberValidateAllureSelenideListenerIsActive.java
@@ -7,7 +7,14 @@ import com.xceptance.neodymium.junit4.NeodymiumCucumberRunner;
 import io.cucumber.junit.CucumberOptions;
 
 @RunWith(NeodymiumCucumberRunner.class)
-@CucumberOptions(features = "src/test/resources/com/xceptance/neodymium/junit4/testclasses/cucumber/CucumberValidateAllureSelenideListenerIsActive.feature", glue = "com/xceptance/neodymium/junit4/testclasses/cucumber", plugin = "null_summary")
+@CucumberOptions(features = "src/test/resources/com/xceptance/neodymium/junit4/testclasses/cucumber/CucumberValidateAllureSelenideListenerIsActive.feature", glue = "com/xceptance/neodymium/junit4/testclasses/cucumber", plugin =
+{
+  // Plugins for generating additional JSON and XML reports.
+  // Is equivalent to cucumber.plugin in cucumber.properties.
+  // The plugin for generating an html report is included in the cucumber.properties file for all test cases.
+  "json:target/cucumber-report/cucumber.json",
+  "junit:target/cucumber-report/cucumber.xml"
+})
 public class CucumberValidateAllureSelenideListenerIsActive
 {
 }
